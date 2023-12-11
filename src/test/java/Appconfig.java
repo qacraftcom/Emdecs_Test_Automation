a09abfe1f5e4d52daa3f3f3d02bd97cf523f1d36
akashQACraft <139763496+akashQACraft@users.noreply.github.com>
2023-12-11 14:30:58 +0530

    Configfile
    Configfile

---
+++ src/test/java/Appconfig.java
@@ -0,0 +1,11 @@
+package config;
+
+import tests.TestDriverActions;
+
+public class Appconfig {
+   public static String environmentName = System.getProperty("env","spotorderpartskonaQA");
+  // public  static String environmentName1=System.getProperty("env","");
+
+   
+
+}
