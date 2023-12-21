2640f45142a8c6c0fa1e851ca567fac778d04d27
myfirstgitde <101536933+myfirstgitde@users.noreply.github.com>
2023-12-21 16:12:11 +0530

    Update some xpath and methods ReportsPage

--- src/test/java/pages/ReportsPage.java
+++ src/test/java/pages/ReportsPage.java
@@ -38,10 +38,10 @@ public class ReportsPage extends TestDriverActions {
     @FindBy(xpath = "(//a[text()='Reports'])[2]")
     WebElement btn_Reports;
 
-    @FindBy(xpath = "(//input[@class='x2b'])[1]")
+    @FindBy(xpath = "//label[contains(text(),'From Date:')]//following::input[1]")
     public static List<WebElement> dateTextBox;
 
-    @FindBy(xpath = "(//input[@class='x2b'])[2]")
+    @FindBy(xpath = "//label[contains(text(),'To Date:')]//following::input[1]")
     public static List<WebElement> toDate;
 
     @FindBy(xpath = "//span[contains(text(),'Run Report')]")
@@ -71,22 +71,25 @@ public class ReportsPage extends TestDriverActions {
     @FindBy(xpath = "//label[contains(text(),'UOM:')]//following::select[1]")
     List<WebElement> uom;
 
-    @FindBy(xpath = "//label[contains(text(),'Employee:')]//following::select")
-    List <WebElement> emp;
+    @FindBy(xpath = "//select[contains(@id,'soc1::content')]")
+    List<WebElement> emp;
 
     @FindBy(xpath = "//label[contains(text(),'Report Type:')]//following::select[1]")
-    List <WebElement> reportType;
+    List<WebElement> reportType;
 
     @FindBy(xpath = "(//label[contains(text(),'Report Specification:')])[1]//following::select[1]")
-    List <WebElement> reportSpecification;
+    List<WebElement> reportSpecification;
 
-    @FindBy(xpath = "//tr[contains(@id,'socWar')]/td[2]/child::select ")
-    List <WebElement> warehouse;
+    @FindBy(xpath = "//tr[contains(@id,'socWar')]/td[2]/child::select")
+    List<WebElement> warehouse;
+
+    @FindBy(xpath = "//input[contains(@id,'itSearchWarehouseSelect::content') and @type='text']")
+    WebElement warehouseIn;
 
     @FindBy(xpath = "//span[contains(text(),'Warehouse Receiving')]")
     WebElement ScrollUpTo;
 
-     @FindBy(xpath ="//select[contains(@name,'socCus')]")
+    @FindBy(xpath ="//select[contains(@name,'socCus')]")
     List<WebElement> unitOwner;
 
     @FindBy(xpath = "//div[@class='toast-item-text']")
@@ -115,7 +118,7 @@ public class ReportsPage extends TestDriverActions {
     /**
      * Click On Reports Categories
      */
-    String reportName = "Counter Sale";
+    String reportName = "Employees";
     String allReports = String.format("//span[contains(text(),'%s')]", reportName);
 
     public void clickOnReportsCategories() throws InterruptedException {
@@ -129,8 +132,8 @@ public class ReportsPage extends TestDriverActions {
     /**
      * Click On Available Reports
      */
-    String availReportName = "Print Counter Sale Invoices";
-    String allAvailReports = String.format("//span[contains(text(),'%s')]", availReportName);
+    String availReportName = "Employee Hours Profit / Loss";
+    String allAvailReports = String.format("//span[starts-with(text(),'%s')]", availReportName);
 
     public void clickOnAvailableReports() throws InterruptedException {
         Thread.sleep(10000);
@@ -138,8 +141,7 @@ public class ReportsPage extends TestDriverActions {
         if (dyelement1.isDisplayed()) {
             dyelement1.click();
             WaitActions.getWaits().loadingWait(loder);
-        }
-        else {
+        } else {
             Thread.sleep(5000);
             WebElementActions.getActions().moveOnTargetElement(ScrollUpTo);
             dyelement1.click();
@@ -160,14 +162,13 @@ public class ReportsPage extends TestDriverActions {
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
         SimpleDateFormat dateFormat1 = new SimpleDateFormat("dd/MM/yyyy HH:mm a");
 
-        if (dateTextBox.size()>=1 && dateTextBox.get(0).getAttribute("placeholder").contains("h:mm a")) {
+        if (dateTextBox.size() >= 1 && dateTextBox.get(0).getAttribute("placeholder").contains("h:mm a")) {
             return dateFormat1.format(resultDate);
         } else {
             return dateFormat.format(resultDate);
         }
     }
-    public String currentDate()
-    {
+    public String currentDate() {
         Calendar calendar = Calendar.getInstance();
         Date resultDate = calendar.getTime();
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
@@ -183,29 +184,31 @@ public class ReportsPage extends TestDriverActions {
 
         WaitActions.getWaits().waitForElementToBeRefreshedAndClickable(credit_Limit.get(0));
         WebElementActions.getActions().inputText(credit_Limit.get(0), "10");
-;    }
+
+    }
     public void readingType() throws InterruptedException {
         WaitActions.getWaits().waitForElementToBeRefreshedAndClickable(reading_Type.get(0));
-        WebElementActions.getActions().elementSelectByVisibilityText(reading_Type.get(0),"FUEL");
+        WebElementActions.getActions().elementSelectByVisibilityText(reading_Type.get(0), "FUEL");
 
         WaitActions.getWaits().waitForElementToBeRefreshedAndClickable(uom.get(0));
-        WebElementActions.getActions().elementSelectByVisibilityText(uom.get(0),"GA");
+        WebElementActions.getActions().elementSelectByVisibilityText(uom.get(0), "GA");
     }
+
     public void employee() throws InterruptedException {
         WaitActions.getWaits().waitForElementToBeRefreshedAndClickable(emp.get(0));
-        WebElementActions.getActions().elementSelectByVisibilityText(emp.get(0),"Adica Test Tech 1 (ADICA1)");
+        WebElementActions.getActions().elementSelectByVisibilityText(emp.get(0), "Adica Test Tech 1 (ADICA1)");
     }
     public void reportType() throws InterruptedException {
         Thread.sleep(3000);
 //        Select report = new Select((WebElement) reading_Type);
 //        report.selectByValue("Balance sheet");
-        if(reportType.get(0).getText().contains("Balance sheet")){
+        if (reportType.get(0).getText().contains("Balance sheet")) {
             WaitActions.getWaits().waitForElementToBeRefreshedAndClickable(reportType.get(0));
             WebElementActions.getActions().elementSelectByVisibilityText(reportType.get(0), "Balance sheet");
             Thread.sleep(3000);
             WaitActions.getWaits().waitForElementToBeRefreshedAndClickable(reportSpecification.get(0));
             WebElementActions.getActions().elementSelectByVisibilityText(reportSpecification.get(0), "Balance Sheet");
-        }else {
+        } else {
             WaitActions.getWaits().waitForElementToBeRefreshedAndClickable(reportType.get(0));
             WebElementActions.getActions().elementSelectByVisibilityText(reportType.get(0), "Income Statement");
             Thread.sleep(3000);
@@ -215,26 +218,26 @@ public class ReportsPage extends TestDriverActions {
     }
     public void wareHouse() throws InterruptedException {
         WaitActions.getWaits().waitForElementToBeRefreshedAndClickable(warehouse.get(0));
-        WebElementActions.getActions().elementSelectByVisibilityText(warehouse.get(0),"11329 WAREHO Warehouse for #11329");
+        WebElementActions.getActions().elementSelectByVisibilityText(warehouse.get(0), "11329 WAREHO Warehouse for #11329");
     }
 
     public void enterDateInTextBox(String dateToEnter) throws InterruptedException {
         Thread.sleep(10000);
 
-        if (dateTextBox.size() >= 1) {
-            WebElement disableDate = dateTextBox.get(0);
-            if (disableDate.isEnabled()) {
+        if (dateTextBox.size() >= 1 || toDate.size() >= 1) {
+            WebElement fromDate = dateTextBox.get(0);
+            if(fromDate.isEnabled()) {
                 dateTextBox.get(0).clear();
                 dateTextBox.get(0).sendKeys(dateToEnter);
                 dateTextBox.get(0).sendKeys(Keys.TAB);
             }
-            TestListener.saveScreenshotPNG(driver);
             WebElement toDateTextBox = toDate.get(0);
             if (toDateTextBox.isEnabled()) {
                 toDate.get(0).clear();
                 toDate.get(0).sendKeys(currentDate());
                 TestListener.saveScreenshotPNG(driver);
             }
+            Thread.sleep(2000);
             if (warehouse.size() >= 1) {
                 this.wareHouse();
                 TestListener.saveScreenshotPNG(driver);
@@ -246,9 +249,9 @@ public class ReportsPage extends TestDriverActions {
                 TestListener.saveScreenshotPNG(driver);
             }
 
-        } else if (reportType.size()>=1 && reportSpecification.size()>=1) {
+        } else if (reportType.size() >= 1 && reportSpecification.size() >= 1) {
             this.reportType();
-            if (year.size() > 0 && accounting_Period.size()>0){
+            if (year.size() > 0 && accounting_Period.size() > 0) {
                 Thread.sleep(5000);
                 Select yearDropdown = new Select(year.get(0));
                 yearDropdown.selectByVisibleText("2021");
@@ -257,8 +260,7 @@ public class ReportsPage extends TestDriverActions {
                 priodeDropdown.selectByVisibleText("2");
                 Thread.sleep(2000);
             }
-
-        } else if (year.size() > 0 && accounting_Period.size()>0){
+        } else if (year.size() > 0 && accounting_Period.size() > 0) {
             Thread.sleep(5000);
             Select yearDropdown = new Select(year.get(0));
             yearDropdown.selectByVisibleText("2021");
@@ -273,7 +275,7 @@ public class ReportsPage extends TestDriverActions {
         } else if (reading_Type.size() >= 1 && uom.size() >= 1) {
             this.readingType();
             TestListener.saveScreenshotPNG(driver);
-        }else if (warehouse.size() >=1) {
+        } else if (warehouse.size() >= 1) {
             this.wareHouse();
             TestListener.saveScreenshotPNG(driver);
         }
@@ -392,94 +394,4 @@ public class ReportsPage extends TestDriverActions {
 
 
 
-
-  /*      // Method to get the latest downloaded file in a directory
-        public String getLatestDownloadedFileName () {
-            File dir = new File(folderPath);
-            File[] files = dir.listFiles((d, name) -> name.endsWith(".xls"));
-            if (files != null && files.length > 0) {
-                return files[files.length - 1].getName();
-            } else {
-                return null;
-            }
-        } */
-       //Thread.sleep(5000);
-
-        // Get Latest file
-        //      String folderPath = "C:\\Users\\pc5-q\\Downloads";
-        //       String folderPath = "C:\\Users\\pc5-q\\Downloads";
-
-     /*  File folder = new File(folderPath);
-
-        File[] files = folder.listFiles();
-        System.out.println("List of File:" + files.length);
-        System.out.println("Latest File Name:" + getLatestFile(files).getName());  */
-
-      /*  File latestFile = getLatestFile(fileNames); */
-
-//        if (fileNames != null) {
-        /*        try (BufferedReader br = new BufferedReader(new FileReader(fileNames))) {
-                    String line;
-                    while ((line = br.readLine()) != null) {
-                        System.out.println(line);
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-           else if (latestFile == null) {
-            System.out.println("File is empty");
-
-            Assert.fail("File is empty");
-        }
-
-
-    } */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//    private static boolean latest(File file) throws IOException {
-//        FileInputStream fis = new FileInputStream(file);
-//        Workbook workbook = WorkbookFactory.create(fis);
-//        Sheet sheet = workbook.getSheetAt(0); // Assuming there is only one sheet
-//        fis.close();
-//        return sheet.getPhysicalNumberOfRows() <= 0;
-//    }
-
-
-// Read PDF form New TAB
-
-
-
-// Verify the content of the PDF
-//    String pdfUrl = driver.getCurrentUrl();
-//    String pdfContent = getPDFContent(pdfUrl);
-//
-//    // Perform verifications based on the PDF content
-//        System.out.println("PDF Content:\n" + pdfContent);
-//
-//
-//    private static <InputStream> String getPDFContent(String pdfUrl) {
-//        try (InputStream inputStream = new URL(pdfUrl).openStream()) {
-//            PDDocument document = PDDocument.load(inputStream);
-//            PDFTextStripper pdfTextStripper = new PDFTextStripper();
-//            return pdfTextStripper.getText(document);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            return null;
-//        }
 
